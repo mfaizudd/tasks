c8f8fdd10bcefe7202ffffe33809869b5c1f1029
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2023-02-10 19:36:56 +0700

    feat: login page

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,8 +1,10 @@
 import Layout from "@/components/Layout";
-import { useAppContext } from "@/context/state";
+import { useAppContext, User } from "@/context/state";
 import { setToken } from "@/lib/api";
 import { useRouter } from "next/router";
 import jwt_decode from "jwt-decode";
+import { useEffect } from "react";
+import Link from "next/link";
 
 interface Claims {
     exp: number,
@@ -13,15 +15,34 @@ interface Claims {
 export default function Login() {
     const { user, setUser } = useAppContext();
     const router = useRouter();
-    if (router.isReady) {
-        const token = router.query.token as string;
-        setToken(token);
-        const decoded = jwt_decode<Claims>(token);
-        setUser({ email: decoded.sub })
-    }
+    useEffect(() => {
+        if (router.isReady) {
+            const token = router.query.token;
+            if (!token) {
+                return;
+            }
+            const tokenString = token as string;
+            setToken(tokenString);
+            const decoded = jwt_decode<Claims>(tokenString);
+            setUser({
+                email: decoded.sub,
+            })
+        }
+    }, [router.isReady])
     return (
         <Layout title="Login">
-            {user ? (<p>Logged in as {user.email}</p>) : (<p>Logging in...</p>)}
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <p className="py-6">
+                            {user ? (`Logged in as ${user.email}`) : (`Logging in...`)}
+                        </p>
+                        <Link href="/">
+                            <button className="btn btn-primary">Starts doing tasks</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
