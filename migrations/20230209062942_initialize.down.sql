67b969ab24791d1e34b04fd033fab705a6f06982
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2023-02-24 19:54:25 +0700

    feat!: db rewrite

--- migrations/20230209062942_initialize.down.sql
+++ migrations/20230209062942_initialize.down.sql
@@ -1,13 +1,9 @@
 -- Add down migration script here
-DROP TABLE IF EXISTS "teacher_invite_codes";
-DROP TABLE IF EXISTS "task_assignments";
-DROP TABLE IF EXISTS "tasks";
-DROP TABLE IF EXISTS "students";
-DROP TABLE IF EXISTS "classrooms";
-DROP TABLE IF EXISTS "user_password";
-DROP TABLE IF EXISTS "user_accounts";
+DROP TABLE "assignment_scores";
+DROP TABLE "assignments";
+DROP TABLE "cohort_students";
+DROP TABLE "students";
+DROP TABLE "cohorts";
 
 DROP EXTENSION IF EXISTS "uuid-ossp";
-DROP TYPE IF EXISTS "user_role";
-DROP TYPE IF EXISTS "user_type";
 
--- migrations/20230209062942_initialize.up.sql
+++ migrations/20230209062942_initialize.up.sql
@@ -1,69 +1,45 @@
 -- Add up migration script here
-CREATE TYPE "user_type" AS ENUM ('email', 'google');
-CREATE TYPE "user_role" AS ENUM ('admin', 'teacher', 'student');
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS "user_accounts" (
+CREATE TABLE IF NOT EXISTS "cohorts" (
     "id" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    "name" VARCHAR(255) NOT NULL DEFAULT '',
-    "email" VARCHAR(255) NOT NULL UNIQUE,
-    "type" "user_type" NOT NULL,
-    "role" "user_role" NOT NULL,
-    "created_at" timestamp with time zone NOT NULL DEFAULT now(),
-    "updated_at" timestamp with time zone NOT NULL DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS "user_password" (
-    "id" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    "user_id" uuid NOT NULL REFERENCES "user_accounts"("id") ON DELETE CASCADE,
-    "password" VARCHAR(255) NOT NULL,
-    "created_at" timestamp with time zone NOT NULL DEFAULT now(),
-    "updated_at" timestamp with time zone NOT NULL DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS "classrooms" (
-    "id" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    "user_id" uuid NOT NULL REFERENCES "user_accounts"("id") ON DELETE RESTRICT,
-    "invite_code" VARCHAR(255) NOT NULL UNIQUE,
     "name" VARCHAR(255) NOT NULL,
-    "description" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
     "created_at" timestamp with time zone NOT NULL DEFAULT now(),
     "updated_at" timestamp with time zone NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS "students" (
     "id" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
     "number" VARCHAR(255) NOT NULL,
-    "user_id" uuid NULL REFERENCES "user_accounts"("id") ON DELETE CASCADE,
-    "classroom_id" uuid NOT NULL REFERENCES "classrooms"("id") ON DELETE CASCADE,
     "created_at" timestamp with time zone NOT NULL DEFAULT now(),
     "updated_at" timestamp with time zone NOT NULL DEFAULT now()
 );
 
+CREATE TABLE IF NOT EXISTS "cohort_students" (
+    "cohort_id" uuid NOT NULL REFERENCES "cohorts"("id") ON DELETE RESTRICT,
+    "student_id" uuid NOT NULL REFERENCES "students"("id") ON DELETE RESTRICT,
+    "created_at" timestamp with time zone NOT NULL DEFAULT now(),
+    "updated_at" timestamp with time zone NOT NULL DEFAULT now(),
+    PRIMARY KEY ("cohort_id", "student_id")
+);
 
-CREATE TABLE IF NOT EXISTS "tasks" (
+CREATE TABLE IF NOT EXISTS "assignments" (
     "id" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    "user_id" uuid NOT NULL REFERENCES "user_accounts"("id") ON DELETE RESTRICT,
-    "classroom_id" uuid NOT NULL REFERENCES "classrooms"("id") ON DELETE RESTRICT,
-    "title" VARCHAR(255) NOT NULL,
+    "cohort_id" uuid NOT NULL REFERENCES "cohorts"("id") ON DELETE RESTRICT,
+    "name" VARCHAR(255) NOT NULL,
     "description" VARCHAR(255) NOT NULL,
-    "score_max" integer NOT NULL,
     "created_at" timestamp with time zone NOT NULL DEFAULT now(),
     "updated_at" timestamp with time zone NOT NULL DEFAULT now()
 );
 
-CREATE TABLE IF NOT EXISTS "task_assignments" (
-    "id" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS "assignment_scores" (
+    "assignment_id" uuid NOT NULL REFERENCES "assignments"("id") ON DELETE CASCADE,
     "student_id" uuid NOT NULL REFERENCES "students"("id") ON DELETE CASCADE,
-    "task_id" uuid NOT NULL REFERENCES "tasks"("id") ON DELETE CASCADE,
     "score" integer NOT NULL,
     "created_at" timestamp with time zone NOT NULL DEFAULT now(),
-    "updated_at" timestamp with time zone NOT NULL DEFAULT now()
+    "updated_at" timestamp with time zone NOT NULL DEFAULT now(),
+    PRIMARY KEY ("assignment_id", "student_id")
 );
 
-CREATE TABLE IF NOT EXISTS "teacher_invite_codes" (
-    "id" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    "code" VARCHAR(255) NOT NULL UNIQUE,
-    "created_at" timestamp with time zone NOT NULL DEFAULT now(),
-    "updated_at" timestamp with time zone NOT NULL DEFAULT now()
-);
